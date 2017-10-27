92f4ebd65d6f587a335bbf5fe08b1040c363916d
Sourav Mandal <sourav2406@gmail.com>
2017-10-27 12:45:49 +0530

    few update in treeNode

--- BinaryTree/treeNode.py
+++ BinaryTree/treeNode.py
@@ -1,7 +1,24 @@
 """New binary tree Node"""
 class Node:
-    def __init__(self,key):
+    def __init__(self, key):
         self.val = key
         self.left = None
         self.right = None
-        
+
+def height(node):
+    if node is None:
+        return 0
+    else:
+        lheight = height(node.left)
+        rheight = height(node.right)
+
+        if lheight > rheight:
+            return lheight+1
+        else:
+            return rheight+1
+
+def printTree(root):
+    if root:
+        print(root.val, end='')
+        printTree(root.left)
+        printTree(root.right)
